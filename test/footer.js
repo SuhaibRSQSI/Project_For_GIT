728a0d86fb3237701811a8a9e1b4611c9f4ef47f
SuhaibRSQSI <mdsuhaibrsqsi@gmail.com>
2023-07-22 16:38:35 +0530

    Update footer.js from local

--- test/footer.js
+++ test/footer.js
@@ -1,4 +1,6 @@
 describe("Footer", () => {
   console.log("from footer");
   console.log("checking for merge conflict from feature1 branch");
+  console.log("checking for merge conflict from feature1 branch on local compouter");
+
 });
